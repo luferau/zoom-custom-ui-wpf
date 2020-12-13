cc056ca97983edc9dd1baa889875f5bfc56f3e09
Aliaksei Luferau <aliaksei.luferau@gmail.com>
2020-12-13 12:42:15 +0300

    Add bat file for zoom libraries receiving

---
+++ get-zoom-libraries.bat
@@ -0,0 +1,8 @@
+set "repo_name=zoom-c-sharp-wrapper"
+set "repo_path=https://github.com/zoom/%repo_name%.git
+set "repo_target_name=zoom-custom-ui-wpf"
+
+cd..
+git clone %repo_path%
+
+xcopy /s/e/y "%repo_name%\bin\" "%repo_target_name%\zoombin\"
